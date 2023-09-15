7ff3583a2191c71488ee8ee7e10a84b025a8f506
Tiaan van der Riel <22614885@sun.ac.za>
2023-09-16 01:26:35 +0900

    Working create new list

--- lib/app/modules/home/widgets/add_list_modal.dart
+++ lib/app/modules/home/widgets/add_list_modal.dart
@@ -20,101 +20,103 @@ class AddList extends StatelessWidget {
     final icons = getIcons();
     return Scaffold(
       //key: homeController.formKey,
-      body: ListView(
-        children: [
-          Padding(
-            padding: EdgeInsets.all(3.0.wp),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                IconButton(
-                  onPressed: () {
-                    Get.back();
-                  },
-                  icon: const Icon(Icons.close),
-                ),
-                TextButton(
-                  onPressed: () {
-                    if (homeController.formKey.currentState!.validate()) {
-                      if (homeController.task.value == null) {
-                        EasyLoading.showError("Please select task type.");
-                      } else {
-                        var success = homeController.addTodo(homeController.editController.text);
-                        if (success) {
-                          EasyLoading.showSuccess("Add Todo item success");
-                        } else {
-                          EasyLoading.showError("Todo item is already exist.");
-                        }
-                      }
-                      homeController.editController.clear();
+      body: Form(
+        key: homeController.formKey,
+        child: ListView(
+          children: [
+            Padding(
+              padding: EdgeInsets.all(3.0.wp),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  IconButton(
+                    onPressed: () {
                       Get.back();
-                    }
-                  },
-                  style: const ButtonStyle(
-                    overlayColor: MaterialStatePropertyAll(Colors.transparent),
+                    },
+                    icon: const Icon(Icons.close),
                   ),
-                  child: Text(
-                    "Done",
-                    style: TextStyle(fontSize: 14.0.sp),
-                  ),
-                )
-              ],
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 5.0.wp),
-            child: Text(
-              "Create List",
-              style: TextStyle(
-                fontSize: 20.0.sp,
-                fontWeight: FontWeight.bold,
+                  TextButton(
+                    onPressed: () {
+                      if (homeController.formKey.currentState!.validate()) {
+                        int icon = icons[homeController.iconChipIndex.value].icon!.codePoint;
+                        String color = icons[homeController.iconChipIndex.value].color!.toHex();
+                        //TODO: Need to remove icon and color here
+                        var task = Task(
+                          title: homeController.editController.text,
+                          icon: icon,
+                          color: color,
+                        );
+                        Get.back();
+                        //TODO: Remove this easyLoading stuff and just use a toast
+                        homeController.addTask(task) ? EasyLoading.showSuccess("Create success") : EasyLoading.showError("Task duplicated");
+                      }
+                    },
+                    style: const ButtonStyle(
+                      overlayColor: MaterialStatePropertyAll(Colors.transparent),
+                    ),
+                    child: Text(
+                      "Done",
+                      style: TextStyle(fontSize: 14.0.sp),
+                    ),
+                  )
+                ],
               ),
             ),
-          ),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 5.0.wp),
-            child: TextFormField(
-              controller: homeController.editController,
-              decoration: InputDecoration(
-                focusedBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(
-                    color: Colors.grey[400]!,
-                  ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 5.0.wp),
+              child: Text(
+                "Create List",
+                style: TextStyle(
+                  fontSize: 20.0.sp,
+                  fontWeight: FontWeight.bold,
                 ),
               ),
-              validator: (value) {
-                if (value == null || value.trim().isEmpty) {
-                  return 'Please give your list a name.';
-                }
-                return null;
-              },
             ),
-          ),
-          //TODO: remove all this icon nonsense
-          Padding(
-            padding: EdgeInsets.symmetric(vertical: 5.0.wp),
-            child: Wrap(
-              spacing: 2.0.wp,
-              children: icons
-                  .map(
-                    (elem) => Obx(() {
-                      final index = icons.indexOf(elem);
-                      return ChoiceChip(
-                        backgroundColor: Colors.white,
-                        selectedColor: Colors.grey.shade200,
-                        pressElevation: 0,
-                        label: elem,
-                        selected: homeController.iconChipIndex.value == index,
-                        onSelected: (bool selected) {
-                          homeController.iconChipIndex.value = selected ? index : 0;
-                        },
-                      );
-                    }),
-                  )
-                  .toList(),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 5.0.wp),
+              child: TextFormField(
+                controller: homeController.editController,
+                decoration: InputDecoration(
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.grey[400]!,
+                    ),
+                  ),
+                ),
+                validator: (value) {
+                  if (value == null || value.trim().isEmpty) {
+                    return 'Please give your list a name.';
+                  }
+                  return null;
+                },
+              ),
             ),
-          ),
-        ],
+            //TODO: remove all this icon nonsense
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 5.0.wp),
+              child: Wrap(
+                spacing: 2.0.wp,
+                children: icons
+                    .map(
+                      (elem) => Obx(() {
+                        final index = icons.indexOf(elem);
+                        return ChoiceChip(
+                          backgroundColor: Colors.white,
+                          selectedColor: Colors.grey.shade200,
+                          pressElevation: 0,
+                          label: elem,
+                          selected: homeController.iconChipIndex.value == index,
+                          onSelected: (bool selected) {
+                            homeController.iconChipIndex.value = selected ? index : 0;
+                          },
+                        );
+                      }),
+                    )
+                    .toList(),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
